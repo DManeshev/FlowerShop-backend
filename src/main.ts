8863b618835bc76fda562590e70da8dd3093044c
Denis Manehshev <d.maneshev@yoursolution.ru>
2024-03-03 12:38:48 +0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -8,11 +8,20 @@ async function bootstrap() {
 	const prismaService = app.get(PrismaService)
 	await prismaService.enableShutdownHooks(app)
 
+	app.use((req, res, next) => {
+		res.header('Access-Control-Allow-Origin', '*')
+		res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+		res.header('Access-Control-Allow-Headers', 'Content-Type, Accept')
+		next()
+	})
+
 	app.setGlobalPrefix('api')
 	app.enableCors({
-		origin: ['http://localhost:3000', 'http://62.109.25.175:3000', 'http://62.109.25.175', 'http://yourflowers21.ru/'],
+		origin: true,
+		// origin: ['http://localhost:3000', 'http://62.109.25.175:3000', 'http://62.109.25.175', 'http://yourflowers21.ru/'],
 		credentials: true,
-		exposedHeaders: 'set-cookie'
+		exposedHeaders: 'set-cookie',
+		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS'
 	})
 
 	await app.listen(4200)
