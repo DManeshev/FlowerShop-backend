73736b8efd160c0032d1254e2b8d4b34d7c3aab2
Denis Manehshev <d.maneshev@yoursolution.ru>
2024-03-03 13:03:17 +0300

    cors(5)

--- src/main.ts
+++ src/main.ts
@@ -10,10 +10,16 @@ async function bootstrap() {
 
 	app.setGlobalPrefix('api')
 	app.enableCors({
-		origin: '*',
-		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+		origin: [
+			'http://localhost:3000',
+			'http://62.109.25.175',
+			'http://62.109.25.175:3000',
+			'http://62.109.25.175:4200',
+			'http://yourflowers21.ru',
+		],
 		credentials: true,
 		exposedHeaders: 'set-cookie',
+		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS'
 	})
 
 	await app.listen(4200)
