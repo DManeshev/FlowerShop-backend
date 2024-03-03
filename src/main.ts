cb6f6643b2520a8ff2ca898bfc1dff511aff3b24
Denis Manehshev <d.maneshev@yoursolution.ru>
2024-03-03 12:50:54 +0300

    cors(3)

--- src/main.ts
+++ src/main.ts
@@ -12,9 +12,10 @@ async function bootstrap() {
 	app.enableCors({
 		origin: [
 			'http://localhost:3000',
-			'http://62.109.25.175:3000',
 			'http://62.109.25.175',
-			'http://yourflowers21.ru'
+			'http://62.109.25.175:3000',
+			'http://62.109.25.175:4200',
+			'http://yourflowers21.ru',
 		],
 		credentials: true,
 		exposedHeaders: 'set-cookie',
